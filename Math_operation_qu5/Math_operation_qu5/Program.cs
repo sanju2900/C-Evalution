36a2af1f4bb47456141e92a3a05f30c4541a88e5
Sanjana Kumari <58020266+sanju2900@users.noreply.github.com>
2022-09-30 13:58:58 +0530

    Add files via upload

---
+++ Math_operation_qu5/Math_operation_qu5/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Write a C# program to calculate the multiplication table and the factorial of the given number.
+Get the number as an input from the user. 
+ 
+ */
+
+
+namespace Math_operation_qu5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double factorial = 1.0;
+            Console.WriteLine("Enter the number for table and factorial : ");
+            int n= Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Table of "+ n +" is : ");
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("n x "+i+ " = "+ n*i);
+            }
+            Console.WriteLine("Factorial of " + n + " is : ");
+            for (int i = 1; i <= n; ++i)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine("Factorial of " + n + " = " + factorial);
+        }
+    }
+}
