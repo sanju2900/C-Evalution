7e40a61a4673e1f7aa111bdbb8d3df8e5e625f51
Sanjana Kumari <58020266+sanju2900@users.noreply.github.com>
2022-09-30 14:05:07 +0530

    Add files via upload

---
+++ Collection_qu7/Collection_qu7/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ Write a C# program that uses a Queue to store a list of people. Create a class called Person.cs with two 
+properties (name and age) and a ToString() method to print the result.
+
+Then define a Queue and request the information (name and age) of 5 people from the user, you will have to 
+store the information in the queue.  Finally print the result of the queue on screen. 
+ */
+
+
+using System.Collections;
+
+namespace Collection_qu7
+{
+    public class Queue_obj
+    {
+        
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+
+            public override string ToString()
+            {
+                return Name + " - " + Age;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Queue list = new Queue();
+            
+
+            int total = 5;
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine("Enter the Name Person: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter the Age : ");
+                int age = int.Parse(Console.ReadLine());
+
+                list.Enqueue(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            foreach (Person p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
